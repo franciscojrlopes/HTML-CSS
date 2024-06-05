2dc50f900dfde76005f06c314017a8043e35500f
franciscojrlopes <fralopes@yahoo.com.br>
2024-06-05 10:24:12 -0300

    Update script.js

--- Exercícios/HTML/Django/script.js
+++ Exercícios/HTML/Django/script.js
@@ -6,3 +6,9 @@ function Perguntar() {
     var nome;
     nome = prompt('Qual é o seu nome?');
     alert("Olá " + nome);}
+
+function mudar_texto(){
+
+    var h1 = document.getElementsByTagName('h1')
+    h1[0].innerText = 'Evolua para Python Django'
+}
