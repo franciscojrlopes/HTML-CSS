cda650f6d9af3a669f9885aaf3341f93f36089fe
franciscojrlopes <fralopes@yahoo.com.br>
2024-06-05 10:45:03 -0300

    Update script.js

--- Exercícios/HTML/Django/script.js
+++ Exercícios/HTML/Django/script.js
@@ -10,5 +10,12 @@ function Perguntar() {
 function mudar_texto(){
 
     var h1 = document.getElementsByTagName('h1')
-    h1[0].innerText = 'Evolua para Python Django'
+    // h1[0].innerText = 'Evolua para Python Django'
+    if (h1[0].innerText == 'JS com Django'){h1[0].innerText = 'Evolu para Python Django';}
+    else{h1[0].innerText = 'JS com Django'}  
+}
+
+function incrementar(){
+    var p1 = document.getElementById('p1');
+    p1.innerText = parseInt(p1.innerText) + 1;
 }
